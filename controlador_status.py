22b6e753a240af67058121c473c11b79e876a93f
Cleverson Borges <143210642+Cleverson-Borges@users.noreply.github.com>
2023-10-12 20:35:35 -0300

    Delete controlador_status.py

--- controlador_status.py
+++
@@ -1,9 +0,0 @@
-from Telas.tela_status import ViewStatus
-
-class StatusController:
-    def __init__(self, controlador_central):
-        self.__controlador_central = controlador_central
-
-
-    def cadastra_status(self):
-

568c7599aa78d412197b6423c08d073e9ad23946
Breno Sayão <101520655+brenisio@users.noreply.github.com>
2023-10-15 11:55:18 -0300

    Update controlador_central.py

--- controlador_central.py
+++ controlador_central.py
@@ -1,14 +1,21 @@
-from controlador_boxeador import BoxeadorController
-from controlador_habilidade import HabilityController
-from controlador_status import StatusController
+from Controladores.controlador_boxeador import ControladorBoxeador
+from Controladores.controlador_habilidade import ControladorHabilidade
+from Controladores.controlador_luta import ControladorLuta
+from Controladores.controlador_torneio import ControladorTorneio
+from Telas.tela_central import TelaCentral
 
 
-class CentralController:
+class ControladorCentral:
     def __init__(self):
-        self.__controlador_boxeador = BoxeadorController()
-        self.__controlador_habilidade = HabilityController()
-        self.__controlador_status = StatusController()
+        self.__controlador_boxeador = ControladorBoxeador(self)
+        self.__controlador_habilidade = ControladorHabilidade(self)
+        self.__controlador_luta = ControladorLuta(self)
+        self.__controlador_torneio = ControladorTorneio(self)
+        self.__tela_central = TelaCentral()
 
+
+    def inicializa_sistema(self):
+        self.abre_tela()
 
     @property
     def controlador_boxeador(self):
@@ -19,5 +26,28 @@ class CentralController:
         return self.__controlador_habilidade
 
     @property
-    def controlador_status(self):
-        return self.__controlador_status
+    def controlador_luta(self):
+        return self.__controlador_luta
+
+    @property
+    def controlador_torneio(self):
+        return self.__controlador_torneio
+
+    def cria_base_dados(self):
+        self.__controlador_boxeador.gerar_boxeador()
+        self.__controlador_habilidade.gerar_base_de_habilidades()
+    def cadastra_boxeador(self):
+        self.__controlador_boxeador.abre_tela()
+        self.cria_base_dados()
+
+    def cadastra_torneio(self):
+        self.__controlador_torneio.abre_tela()
+
+    def abre_tela(self):
+        lista_opcoes = {1: self.cadastra_boxeador,
+                        2: self.__controlador_torneio.abre_tela,
+                        3: self.__controlador_luta.iniciar_luta}
+        while True:
+            opcao_escolhida = self.__tela_central.tela_opcoes()
+            funcao_escolhida = lista_opcoes[opcao_escolhida]
+            funcao_escolhida()

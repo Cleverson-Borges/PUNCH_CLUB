fc926618cd15dbb1de847d2cd5e8cd16881c7ded
Breno Sayão <101520655+brenisio@users.noreply.github.com>
2023-10-15 11:56:34 -0300

    Update controlador_torneio.py

--- controlador_torneio.py
+++ controlador_torneio.py
@@ -1,53 +1,78 @@
-from Entidades.torneio import Torneio
 from Telas.tela_torneio import TelaTorneio
 from Entidades.luta import Luta
-from Entidades.boxeador import Boxeador
-from Controladores.controlador_boxeador import ControleBoxeador
+from Entidades.torneio import Torneio
 import random
 
 
 class ControladorTorneio:
-    def __init__(self, controlador_central, nome_torneio: str, numero_lutadores: int):
+    def __init__(self, controlador_central):
         self.__controlador_central = controlador_central
-        self.__nome_torneio = nome_torneio
-        self.__numero_lutadores = numero_lutadores
-        self.__boxeadores = ControleBoxeador.boxeadores
-        self.__tela_torneio = TelaTorneio
+        self.__tela_torneio = TelaTorneio()
+        self.__torneio_atual = None
         self.__lutas = []
+        self.__torneio_criado = False
 
     @property
-    def nome_torneio(self):
-        return self.__nome_torneio
-
-    @property
-    def numero_lutadores(self):
-        return self.__numero_lutadores
-
-    @nome_torneio.setter
-    def nome_torneio(self, nome):
-        self.__nome_torneio = nome
-
-    @numero_lutadores.setter
-    def numero_lutadores(self, numero):
-        self.__numero_lutadores = numero
-
-    def verifica_entrada_lutadores(self, num):
-        while not(num == 4) or (num == 8):
-            print("Entrada Inválida")
-            num = input(int("Informe novamente o numero de lutadores que dejsa no torneio (4 ou 8): "))
-        return num
+    def lutas_torneio(self):
+        return self.__lutas
 
     def cadastrar_torneio(self):
-        pass
+        if self.__torneio_criado:
+            self.__tela_torneio.mostra_mensagem("Torneio já cadastrado, se quiser altere-o!")
+        else:
+            informacoes_torneio = self.__tela_torneio.cadastro_torneio()
+            nome_torneio = informacoes_torneio["nome_torneio"]
+            numero_lutadores = informacoes_torneio["numero_lutadores"]
+            self.__torneio_atual = Torneio(nome_torneio, numero_lutadores)
+            self.criar_lutas()
+            self.__torneio_criado = True
 
-    def inscrever_lutador(self):
-        pass
+    def alterar_torneio(self):
+        if self.__torneio_atual:
+            informacoes_torneio = self.__tela_torneio.cadastro_torneio()
+            nome_torneio = informacoes_torneio["nome_torneio"]
+            numero_lutadores = informacoes_torneio["numero_lutadores"]
+            self.__torneio_atual.nome_torneio = nome_torneio
+            self.__torneio_atual.numero_lutadores = numero_lutadores
+        else:
+            self.__tela_torneio.mostra_mensagem("Não há nenhum torneio cadastrado para editar")
+    def mostrar_torneio(self):
+        if self.__torneio_atual:
+            self.__tela_torneio.mostrar_torneio(self.__torneio_atual.nome_torneio, self.__torneio_atual.numero_lutadores)
+            for luta in self.__lutas:
+                self.__tela_torneio.mostrar_chaveamento(luta.boxeador_um, luta.lutador_dois)
+        else:
+            self.__tela_torneio.mostra_mensagem("Torneio não cadastrado, é necessário cadastrar o torneio antes")
+    def criar_lutas(self):
+        lista_boxeadores = self.__controlador_central.controlador_boxeador.boxeadores
+        for i in range(len(lista_boxeadores) - 1):
+            lutador_um = lista_boxeadores[i]
+            lutador_dois = lista_boxeadores[i + 1]
+            self.__lutas.append(Luta(lutador_um, lutador_dois))
 
-    def gerar_chaveamento(self):
-        self.__tela_torneio.mostrar_chveamento(self.__nome_torneio, self.__boxeadores)
-
+    def mostrar_luta_usuario(self):
+        self.__tela_torneio.mostrar_luta_usuario()
 
     def gerar_lutas(self):
-        for boxeador in range(self.__numero_lutadores):
-            boxeador1, boxeador2 = random.sample(self.__boxeadores, 2)
+        lista_boxeadores = self.__controlador_central.controlador_boxeador.boxeadores
+        for boxeador in range(lista_boxeadores):
+            boxeador1, boxeador2 = random.sample(lista_boxeadores, 2)
             return boxeador1, boxeador2
+
+    def mostrar_chaveamento(self):
+        for luta in self.__lutas:
+            self.__tela_torneio.mostrar_chaveamento(self.__torneio_atual.nome_torneio, luta.boxeador_um, luta.boxeador_dois, luta.fase)
+
+    def encerra_sistema(self):
+        self.__controlador_central.abre_tela()
+
+    def abre_tela(self):
+
+        escolha_tela = {1: self.cadastrar_torneio,
+                        2: self.alterar_torneio,
+                        3: self.mostrar_torneio,
+                        0: self.encerra_sistema}
+
+        continua = True
+        while continua:
+            escolha_tela[self.__tela_torneio.tela_opcoes()]()

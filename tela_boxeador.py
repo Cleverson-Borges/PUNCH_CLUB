247a92f218b1ccf0394b788abe34a66c1b17c8f4
Breno Sayão <101520655+brenisio@users.noreply.github.com>
2023-10-15 11:59:01 -0300

    Update tela_boxeador.py

--- tela_boxeador.py
+++ tela_boxeador.py
@@ -1,12 +1,20 @@
-from Entidades.boxeador import Boxeador
-
-
-class ViewBoxeador:
-    def __init__(self, controlador_central):
+class TelaBoxeador:
+    def __init__(self):
         pass
 
-
-    def tela_cadastro_boxeador(self):
+    def le_num_inteiro(self, mensagem=" ", ints_validos=None):
+        while True:
+            valor_lido = input(mensagem)
+            try:
+                valor_int = int(valor_lido)
+                if ints_validos and valor_int not in ints_validos:
+                    raise ValueError
+                return valor_int
+            except ValueError:
+                print("Valor incorreto!")
+                if ints_validos:
+                    print("Valores válidos: ", ints_validos)
+    def cadastrar_boxeador(self):
         print('Cadastro de boxeador')
         nome = input('Nome: ')
         apelido = input('Apelido: ')
@@ -14,13 +22,41 @@ class ViewBoxeador:
         peso = float(input('Peso: '))
         altura = float(input('Altura: '))
         nacionalidade = input('Nacionalidade: ')
-        equipamento = input('Equipamento: ')
-        status = input('Status: ')
         return {'nome': nome,
                 'apelido': apelido,
                 'idade': idade,
                 'peso': peso,
                 'altura': altura,
-                'nacionalidade': nacionalidade,
-                'equipamento': equipamento,
-                'status': status}
+                'nacionalidade': nacionalidade}
+
+    def tela_opcoes(self):
+        print('-------- ░▒▓█ CADASTRO DE BOXEADOR █▓▒░ ----------',)
+        print('Escolha sua opção:')
+        print('(1) Incluir Boxeador')
+        print('(2) Alterar Boxeador')
+        print('(3) Listar Boxeador')
+        print('(4) Excluir Boxeador')
+        print('(0) Retornar')
+        opcao = self.le_num_inteiro("Escolha a opcao: ", [0, 1, 2, 3, 4])
+        return opcao
+
+    def mostrar_mensagem(self, mensagem):
+        print(mensagem)
+
+    def obtem_cpf(self):
+        cpf = int(input('Por favor, insira o CPF: '))
+        return cpf
+
+    def verifica_boxeador_cpu(self):
+        resposta = input('É o seu lutador ou é da CPU? (Responda com S (Sim) ou N (Não): ')
+        return resposta
+
+    def mostra_boxeador(self, dados_boxeador):
+        print(f'NOME DO BOXEADOR: {dados_boxeador["nome"]}')
+        print(f'CPF: {dados_boxeador["cpf"]}')
+        print(f'APELIDO: {dados_boxeador["apelido"]}')
+        print(f'IDADE: {dados_boxeador["idade"]} anos')
+        print(f'PESO: {dados_boxeador["peso"]} kg')
+        print(f'ALTURA: {dados_boxeador["altura"]} m')
+        print(f'NACIONALIDADE: {dados_boxeador["nacionalidade"]}')
+        print()

f556b31f9a7a870efcf581a98caa9f253c18f5f9
Cleverson Borges <143210642+Cleverson-Borges@users.noreply.github.com>
2023-11-23 17:10:35 -0300

    Update tela_boxeador.py

--- Telas/tela_boxeador.py
+++ Telas/tela_boxeador.py
@@ -4,57 +4,8 @@ import PySimpleGUI as sg
 
 class TelaBoxeador(TelaAbstrata):
     def __init__(self):
+        self.__window = None
         self.tela_opcoes()
-
-    def le_num_inteiro(self, mensagem=" ", ints_validos=None):
-        while True:
-            valor_lido = input(mensagem)
-            try:
-                valor_int = int(valor_lido)
-                if ints_validos and valor_int not in ints_validos:
-                    raise ValueError
-                return valor_int
-            except ValueError:
-                print("Valor inválido!")
-                if ints_validos:
-                    print("Valores válidos: ", ints_validos)
-
-    def le_num_inteiro_entre(self, mensagem=" ", min_valor=None, max_valor=None):
-        while True:
-            valor_lido = input(mensagem)
-            try:
-                valor_int = int(valor_lido)
-                if (min_valor is not None and valor_int < min_valor) or (
-                        max_valor is not None and valor_int > max_valor):
-                    raise ValueError
-                return valor_int
-            except ValueError:
-                print(f"Você deve ter no minímo {min_valor} anos e no maximo {max_valor} anos para poder participar.")
-
-    def le_num_float_entre(self, mensagem=" ", min_valor=None, max_valor=None):
-        while True:
-            valor_lido = input(mensagem)
-            try:
-                valor_float = float(valor_lido)
-                if (min_valor is not None and valor_float < min_valor) or (
-                        max_valor is not None and valor_float > max_valor):
-                    raise ValueError
-                return valor_float
-            except ValueError:
-                print(f"O valor deve estar entre {min_valor} e {max_valor}.")
-
-    def le_str_valida(self, mensagem=" ", strs_validas=None):
-        while True:
-            valor_lido = input(mensagem)
-            if len(valor_lido) == 3:
-                if strs_validas and valor_lido not in strs_validas:
-                    print("Valor incorreto!")
-                    if strs_validas:
-                        print("Valores válidos: ", ", ".join(strs_validas))
-                else:
-                    return valor_lido.upper()
-            else:
-                print("A string deve conter exatamente 3 letras.")
 
     def open(self):
         button, values = self.__window.Read()
@@ -90,7 +41,17 @@ class TelaBoxeador(TelaAbstrata):
         ]
         self.__window = sg.Window('Cadastamento boxeador').Layout(layout)
 
+        button, values = self.open()
+        nome = values['nome']
+        apelido = values['apelido']
+        idade = values['idade']
+        peso = values['peso']
+        altura = values['altura']
+        nacionalidade = values['naconalidade']
 
+        self.close()
+        return {"nome": nome, "apelido": apelido, "idade": idade, "peso": peso, "altura": altura,
+                "nacionalidade": nacionalidade}
 
     def tela_opcoes(self):
         sg.ChangeLookAndFeel('DarkTanBlue')
@@ -99,21 +60,18 @@ class TelaBoxeador(TelaAbstrata):
                 [
                     [sg.Text('░▒▓█ CADASTRO DE BOXEADOR █▓▒░', font=("Lucida", 25, 'bold'))],
                     [sg.Text('Escolha sua opção:', font=("Lucida", 15, 'bold'))],
-                    [sg.Button('Incluir boxeador', size=(20, 2), key='1', button_color=('white', 'black'), border_width=0,
-                               font=("Lucida", 12, 'bold'))],
-                    [sg.Button('Alterar boxeador', size=(20, 2), key='2', button_color=('white', 'black'), border_width=0,
-                               font=("Lucida", 12, 'bold'))],
-                    [sg.Button('Listar boxeadores', size=(20, 2), key='3', button_color=('white', 'black'), border_width=0,
-                               font=("Lucida", 12, 'bold'))],
+                    [sg.Button('Incluir boxeador', size=(20, 2), key='1', button_color=('white', 'black'),
+                               border_width=0,font=("Lucida", 12, 'bold'))],
+                    [sg.Button('Alterar boxeador', size=(20, 2), key='2', button_color=('white', 'black'),
+                               border_width=0, font=("Lucida", 12, 'bold'))],
+                    [sg.Button('Listar boxeadores', size=(20, 2), key='3', button_color=('white', 'black'),
+                               border_width=0, font=("Lucida", 12, 'bold'))],
                     [sg.Button('Excluir boxeador', size=(20, 2), key='4', button_color=('white', 'black'),
-                               border_width=0,
-                               font=("Lucida", 12, 'bold'))],
+                               border_width=0, font=("Lucida", 12, 'bold'))],
                     [sg.Button('Listar habilidades boxeador', size=(20, 2), key='5', button_color=('white', 'black'),
-                               border_width=0,
-                               font=("Lucida", 12, 'bold'))],
-                    [sg.Button('Listar caracteristicas boxeador', size=(20, 2), key='6', button_color=('white', 'black'),
-                               border_width=0,
-                               font=("Lucida", 12, 'bold'))],
+                               border_width=0, font=("Lucida", 12, 'bold'))],
+                    [sg.Button('Listar caracteristicas boxeador', size=(20, 2), key='6',
+                               button_color=('white', 'black'), border_width=0, font=("Lucida", 12, 'bold'))],
                     [sg.Button('Confirmar', size=(8, 1), button_color=('black', 'green'),
                                font=("Lucida", 12, 'bold')),
                      sg.Cancel('Voltar', size=(8, 1), key='0', button_color=('black', 'red'),
@@ -178,6 +136,18 @@ class TelaBoxeador(TelaAbstrata):
     def verifica_boxeador_cpu(self):
         resposta = input('Este é o lutador que você quer jogar? (Responda com S (Sim) ou N (Não): ')
         return resposta
+
+    def tela_cpu(self):
+        layout = [
+            [sg.Text('Este é o seu lutador??')],
+            [sg.Button('Sim'), sg.Button('Não')]
+        ]
+        self.__window = sg.Window('boxeador usuario').Layout(layout)
+
+        button, values = self.open()
+        cpu = values['cpu']
+        self.close()
+        return cpu
 
     def mostra_boxeador(self, dados_boxeador):
         print()

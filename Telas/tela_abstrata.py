faa70b8ec4fd1e0a642f86cbb77e9463044fff97
Cleverson Borges <143210642+Cleverson-Borges@users.noreply.github.com>
2023-11-23 17:14:11 -0300

    Update tela_abstrata.py

--- Telas/tela_abstrata.py
+++ Telas/tela_abstrata.py
@@ -1,10 +1,14 @@
 from abc import ABC, abstractmethod
+import PySimpleGUI as sg
 
 
 class TelaAbstrata(ABC):
     @abstractmethod
     def __init__(self):
         pass
+
+    def print_mensagem(self, mensagem):
+        sg.popup('', mensagem)
 
     def le_num_inteiro(self, mensagem=" ", ints_validos=None):
         while True:
@@ -15,9 +19,9 @@ class TelaAbstrata(ABC):
                     raise ValueError
                 return valor_int
             except ValueError:
-                print("Valor inválido!")
+                sg.popup('Valor Invalido!')
                 if ints_validos:
-                    print("Valores válidos: ", ints_validos)
+                    sg.popup('Valores válidos', ints_validos)
 
     def le_num_inteiro_entre(self, mensagem=" ", min_valor=None, max_valor=None):
         while True:
@@ -29,7 +33,8 @@ class TelaAbstrata(ABC):
                     raise ValueError
                 return valor_int
             except ValueError:
-                print(f"Você deve ter no minímo {min_valor} anos e no maximo {max_valor} anos para poder participar.")
+                sg.popup(f'Você deve ter no minímo {min_valor} anos e no maximo {max_valor} '
+                         f'anos para poder participar.')
 
     def le_num_float_entre(self, mensagem=" ", min_valor=None, max_valor=None):
         while True:
@@ -41,17 +46,17 @@ class TelaAbstrata(ABC):
                     raise ValueError
                 return valor_float
             except ValueError:
-                print(f"O valor deve estar entre {min_valor} e {max_valor}.")
+                sg.popup(f"O valor deve estar entre {min_valor} e {max_valor}.")
 
     def le_str_valida(self, mensagem=" ", strs_validas=None):
         while True:
             valor_lido = input(mensagem)
             if len(valor_lido) == 3:
                 if strs_validas and valor_lido not in strs_validas:
-                    print("Valor incorreto!")
+                    sg.popup("Valor incorreto!")
                     if strs_validas:
-                        print("Valores válidos: ", ", ".join(strs_validas))
+                        sg.popup("Valores válidos: ", ", ".join(strs_validas))
                 else:
                     return valor_lido.upper()
             else:
-                print("A string deve conter exatamente 3 letras.")
+                sg.popup("A string deve conter exatamente 3 letras.")

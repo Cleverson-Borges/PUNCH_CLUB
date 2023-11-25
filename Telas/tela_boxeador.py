ec42ab53655674984db7a6f780345e8c0d84120d
Breno Sayão <101520655+brenisio@users.noreply.github.com>
2023-11-25 18:51:35 -0300

    Update tela_boxeador.py

--- Telas/tela_boxeador.py
+++ Telas/tela_boxeador.py
@@ -4,73 +4,38 @@ import PySimpleGUI as sg
 
 class TelaBoxeador(TelaAbstrata):
     def __init__(self):
+        self.__window = None
         self.tela_opcoes()
 
-    def le_num_inteiro(self, mensagem=" ", ints_validos=None):
-        while True:
-            valor_lido = input(mensagem)
-            try:
-                valor_int = int(valor_lido)
-                if ints_validos and valor_int not in ints_validos:
-                    raise ValueError
-                return valor_int
-            except ValueError:
-                print("Valor inválido!")
-                if ints_validos:
-                    print("Valores válidos: ", ints_validos)
-
-    def le_num_inteiro_entre(self, mensagem=" ", min_valor=None, max_valor=None):
-        while True:
-            valor_lido = input(mensagem)
-            try:
-                valor_int = int(valor_lido)
-                if (min_valor is not None and valor_int < min_valor) or (
-                        max_valor is not None and valor_int > max_valor):
-                    raise ValueError
-                return valor_int
-            except ValueError:
-                print(f"Você deve ter no minímo {min_valor} anos e no maximo {max_valor} anos para poder participar.")
-
-    def le_num_float_entre(self, mensagem=" ", min_valor=None, max_valor=None):
-        while True:
-            valor_lido = input(mensagem)
-            try:
-                valor_float = float(valor_lido)
-                if (min_valor is not None and valor_float < min_valor) or (
-                        max_valor is not None and valor_float > max_valor):
-                    raise ValueError
-                return valor_float
-            except ValueError:
-                print(f"O valor deve estar entre {min_valor} e {max_valor}.")
-
-    def le_str_valida(self, mensagem=" ", strs_validas=None):
-        while True:
-            valor_lido = input(mensagem)
-            if len(valor_lido) == 3:
-                if strs_validas and valor_lido not in strs_validas:
-                    print("Valor incorreto!")
-                    if strs_validas:
-                        print("Valores válidos: ", ", ".join(strs_validas))
-                else:
-                    return valor_lido.upper()
-            else:
-                print("A string deve conter exatamente 3 letras.")
+    def open(self):
+        button, values = self.__window.Read()
+        return button, values
 
     def cadastrar_boxeador(self):
-        print('Cadastro de boxeador')
-        nome = str(input('Nome: '))
-        apelido = str(input('Apelido: '))
-        idade = self.le_num_inteiro_entre('Idade: ', 18, 100)
-        peso = self.le_num_float_entre('Peso: ', 30, 150)
-        altura = self.le_num_float_entre('Altura [metros]: ', 1.20, 2.30)
-        nacionalidade = self.le_str_valida('Nacionalidade: ')
-        print()
-        return {'nome': nome,
-                'apelido': apelido,
-                'idade': idade,
-                'peso': peso,
-                'altura': altura,
-                'nacionalidade': nacionalidade}
+        sg.ChangeLookAndFeel('DarkTanBlue')
+        layout = [
+                    [sg.Text('░▒▓█ CADASTRAMENTO DE NOVO BOXEADOR █▓▒░', font=('Lucida', 25, 'bold'))],
+                    [sg.Text('Nome:', size=(15, 1)), sg.InputText('', key='nome')],
+                    [sg.Text('Apelido:', size=(15, 1)), sg.InputText('', key='apelido')],
+                    [sg.Text('Idade:', size=(15, 1)), sg.InputText('', key='idade')],
+                    [sg.Text('Peso:', size=(15, 1)), sg.InputText('', key='peso')],
+                    [sg.Text('Altura:', size=(15, 1)), sg.InputText('', key='altura')],
+                    [sg.Text('Nacionalidade:', size=(15, 1)), sg.InputText('', key='nacionalidade')],
+                    [sg.Button('Cadastrar'), sg.Cancel('Voltar')]
+        ]
+        self.__window = sg.Window('Cadastamento boxeador').Layout(layout)
+
+        button, values = self.open()
+        nome = values['nome']
+        apelido = values['apelido']
+        idade = values['idade']
+        peso = values['peso']
+        altura = values['altura']
+        nacionalidade = values['naconalidade']
+
+        self.close()
+        return {"nome": nome, "apelido": apelido, "idade": idade, "peso": peso, "altura": altura,
+                "nacionalidade": nacionalidade}
 
     def tela_opcoes(self):
         sg.ChangeLookAndFeel('DarkTanBlue')
@@ -126,66 +91,75 @@ class TelaBoxeador(TelaAbstrata):
         self.close()
         return opcao
 
-        if button == '0' in (None, 'Cancelar'):
-            opcao = 0
-
-        self.close()
-        return opcao
-
     def mostrar_mensagem(self, mensagem):
-        print(mensagem)
+        sg.popup('', mensagem)
 
     def obtem_cpf(self):
-        cpf = self.le_num_inteiro('Por favor, insira o CPF: ')
-        print()
-        return cpf
+        sg.ChangeLookAndFeel('DarkTanBlue')
+        layout = [
+            [sg.Text(' ░▒▓█ SELECIONAR BOXEADOR █▓▒░ ', font=("Lucida", 25))],
+            [sg.Text('Digite o CPF do boxeador que deseja selecionar:', font=("Lucida", 15))],
+            [sg.Text('CPF:', size=(15, 1)), sg.InputText('', key='cpf')],
+            [sg.Button('Confirmar'), sg.Cancel('Voltar')]
+        ]
+        self.__window = sg.Window('Seleciona boxeador').Layout(layout)
+
+        button, values = self.open()
+        cpf = values['cpf']
+        self.close()
+
+        for valor in values:
+            if button == 'Voltar':
+                self.close()
+                return 0
+            if values[valor] == '':
+                self.close()
+                return False
+
+        return int(cpf)
 
     def verifica_boxeador_cpu(self):
         resposta = input('Este é o lutador que você quer jogar? (Responda com S (Sim) ou N (Não): ')
         return resposta
 
     def mostra_boxeador(self, dados_boxeador):
-        print()
-        print(f'NOME DO BOXEADOR: {dados_boxeador["nome"]}')
-        print(f'CPF: {dados_boxeador["cpf"]}')
-        print(f'APELIDO: {dados_boxeador["apelido"]}')
-        print(f'IDADE: {dados_boxeador["idade"]} anos')
-        print(f'PESO: {dados_boxeador["peso"]} kg')
-        print(f'ALTURA: {dados_boxeador["altura"]} m')
-        print(f'NACIONALIDADE: {dados_boxeador["nacionalidade"]}')
-        print()
+        string_todos_boxeadores = ""
+        for boxeador in dados_boxeador:
+            string_todos_boxeadores = string_todos_boxeadores + "NOME DO BOXEADOR: " + boxeador["nome"] + '\n'
+            string_todos_boxeadores = string_todos_boxeadores + "CPF DO BOXEADOR: " + str(boxeador["cpf"]) + '\n'
+            string_todos_boxeadores = string_todos_boxeadores + "APELIDO: " + boxeador["apelido"] + '\n'
+            string_todos_boxeadores = string_todos_boxeadores + '\n'
 
-    def mostrar_habilidade_ataque(self, habilidade):
-        print(f"ID da habilidade: {habilidade.id}")
-        print(f"Nome: {habilidade.nome}")
-        print(f"Descrição: {habilidade.descricao}")
-        print(f"Custo de Stamina: {habilidade.custo}")
-        print(f"Dano da habilidade: {habilidade.dano}")
-        print()
+        sg.Popup('░▒▓█ LISTA DE BOXEADORES █▓▒░', string_todos_boxeadores)
 
-    def mostrar_habilidade_defesa(self, habilidade):
-        print(f"ID da habilidade: {habilidade.id}")
-        print(f"Nome: {habilidade.nome}")
-        print(f"Descrição: {habilidade.descricao}")
-        print(f"Custo de Stamina: {habilidade.custo}")
-        print(f"Taxa de Defesa: {habilidade.taxa_defesa}")
-        print()
+    def lista_habilidades(self, dados_habilidade):
+        string_todas_habilidades = ""
+        for habilidade in dados_habilidade:
+            string_todas_habilidades = string_todas_habilidades + "ID DA HABILIDADE: " + str(habilidade["id"]) + '\n'
+            string_todas_habilidades = string_todas_habilidades + "NOME DA HABILIDADE: " + habilidade["nome"] + '\n'
+            string_todas_habilidades = string_todas_habilidades + "DESCRIÇÃO DA HABILIDADE: " + habilidade["descricao"] + '\n'
+            string_todas_habilidades = string_todas_habilidades + "CUSTO DE STAMINA: " + str(habilidade["custo"]) + '\n'
+            if habilidade['tipo'] == 'Ataque':
+                string_todas_habilidades = string_todas_habilidades + "DANO DA HABILIDADE: " + str(habilidade["dano"]) + '\n'
+            if habilidade['tipo'] == 'Defesa':
+                string_todas_habilidades = string_todas_habilidades + "TAXA DE DEFESA: " + str(habilidade["taxa_defesa"]) + '\n'
+            if habilidade['tipo'] == 'Esquiva':
+                string_todas_habilidades = string_todas_habilidades + "TAXA DE ESQUIVA: " + str(habilidade["taxa_esquiva"]) + '\n'
+            string_todas_habilidades = string_todas_habilidades + '\n'
 
-    def mostrar_habilidade_esquiva(self, habilidade):
-        print(f"ID da habilidade: {habilidade.id}")
-        print(f"Nome: {habilidade.nome}")
-        print(f"Descrição: {habilidade.descricao}")
-        print(f"Custo de Stamina: {habilidade.custo}")
-        print(f"Taxa de esquiva: {habilidade.taxa_esquiva}")
-        print()
+        sg.Popup('░▒▓█ LISTA DE HABILIDADES DO BOXEADOR █▓▒░', string_todas_habilidades)
 
-    def mostrar_caracteristica_boxeador(self, caracteristica):
-        print(f"Stamina: {caracteristica.stamina}")
-        print(f"Esquiva: {caracteristica.esquiva}")
-        print(f"Vida: {caracteristica.vida}")
-        print(f"Força: {caracteristica.forca}")
-        print(f"Defesa: {caracteristica.defesa}")
-        print()
+    def mostrar_caracteristica_boxeador(self, dados_caracteristica):
+        string_todas_caracteristicas = ""
+        for caracteristica in dados_caracteristica:
+            string_todas_caracteristicas = string_todas_caracteristicas + "FORÇA: " + str(caracteristica["forca"]) + '\n'
+            string_todas_caracteristicas = string_todas_caracteristicas + "ESQUIVA: " + str(caracteristica["esquiva"]) + '\n'
+            string_todas_caracteristicas = string_todas_caracteristicas + "VIDA: " + str(caracteristica["vida"]) + '\n'
+            string_todas_caracteristicas = string_todas_caracteristicas + "STAMINA: " + str(caracteristica["stamina"]) + '\n'
+            string_todas_caracteristicas = string_todas_caracteristicas + "DEFESA: " + str(caracteristica["defesa"]) + '\n'
 
+            string_todas_caracteristicas = string_todas_caracteristicas + '\n'
+
+        sg.Popup('░▒▓█ LISTA DE CARACTERISTICAS DO BOXEADOR █▓▒░', string_todas_caracteristicas)
     def close(self):
         self.__window.Close()

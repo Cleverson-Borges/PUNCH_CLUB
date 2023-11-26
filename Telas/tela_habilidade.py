e69a09309390d296c45280063268966a3895a983
Cleverson Borges <143210642+Cleverson-Borges@users.noreply.github.com>
2023-11-26 16:17:20 -0300

    Update tela_habilidade.py

--- Telas/tela_habilidade.py
+++ Telas/tela_habilidade.py
@@ -1,76 +1,140 @@
 from Telas.tela_central import TelaAbstrata
+import PySimpleGUI as sg
 
 
 class TelaHabilidade(TelaAbstrata):
     def __init__(self):
-        pass
+        self.__window = None
 
-    def le_num_inteiro(self, mensagem=" ", ints_validos=None):
-        while True:
-            valor_lido = input(mensagem)
-            try:
-                valor_int = int(valor_lido)
-                if ints_validos and valor_int not in ints_validos:
-                    raise ValueError
-                return valor_int
-            except ValueError:
-                print("Valor inválido!")
-                if ints_validos:
-                    print("Valores válidos: ", ints_validos)
+    def open(self):
+        button, values = self.__window.Read()
+        return button, values
 
-    def selecionar_tipo(self):
-        print()
-        print('-'*5, "SELEÇÃO DE HABILIDADES!", "-"*5)
-        print("(1) Habilidades de ataque")
-        print("(2) Habilidades de defesa")
-        print("(3) Habilidades de esquiva")
-        print("\n(0) Voltar")
-        print()
-        escolha = self.le_num_inteiro("Escolha uma opção: ", [0, 1, 2, 3])
-        print()
-        return escolha
-
-    def mostrar_habilidade_ataque(self, habilidade):
-        print(f"ID da habilidade: {habilidade.id}")
-        print(f"Nome: {habilidade.nome}")
-        print(f"Descrição: {habilidade.descricao}")
-        print(f"Custo de Stamina: {habilidade.custo}")
-        print(f"Dano da habilidade: {habilidade.dano}")
-        print()
-
-    def mostrar_habilidade_defesa(self, habilidade):
-        print(f"ID da habilidade: {habilidade.id}")
-        print(f"Nome: {habilidade.nome}")
-        print(f"Descrição: {habilidade.descricao}")
-        print(f"Custo de Stamina: {habilidade.custo}")
-        print(f"Taxa de Defesa: {habilidade.taxa_defesa}")
-        print()
-
-    def mostrar_habilidade_esquiva(self, habilidade):
-        print(f"ID da habilidade: {habilidade.id}")
-        print(f"Nome: {habilidade.nome}")
-        print(f"Descrição: {habilidade.descricao}")
-        print(f"Custo de Stamina: {habilidade.custo}")
-        print(f"Taxa de esquiva: {habilidade.taxa_esquiva}")
-        print()
+    def close(self):
+        self.__window.Close()
 
     def mostrar_mensagem(self, mensagem):
         print(mensagem)
 
+    def print_mensagem(self, mensagem):
+        sg.popup('', mensagem)
+
+    def tela_tipo(self):
+        sg.ChangeLookAndFeel('DarkTanBlue')
+        layout = [
+            [sg.Column(
+                [
+                    [sg.Text('   ░▒▓█ SELEÇÃO DE HABILIDADES █▓▒░   ', font=("Lucida", 25, 'bold'))],
+                    [sg.Text('                                      ', font=("Lucida", 15, 'bold'))],
+                    [sg.Button('Habilidades de Ataque', size=(20, 2), key='1', button_color=('white', 'black'),
+                               border_width=0, font=("Lucida", 12, 'bold'))],
+                    [sg.Button('Habilidades de Defesa', size=(20, 2), key='2', button_color=('white', 'black'),
+                               border_width=0, font=("Lucida", 12, 'bold'))],
+                    [sg.Button('Habilidades de Esquiva', size=(20, 2), key='3', button_color=('white', 'black'),
+                               border_width=0, font=("Lucida", 12, 'bold'))],
+                    [sg.Cancel('SAIR', size=(20, 2), key='4', font=("Lucida", 12, 'bold'))],
+                ],
+                element_justification='center'
+            )]
+        ]
+        self.__window = sg.Window('Selecionar de Habilidades').Layout(layout)
+
+
+    def selecionar_tipo(self):
+        self.tela_tipo()
+        button, values = self.__window.Read()
+        opcao = 0
+        if button == '1':
+            opcao = 1
+        if button == '2':
+            opcao = 2
+        if button == '3':
+            opcao = 3
+        if button == '4':
+            opcao = 4
+
+        self.close()
+
+        return opcao
+
+    def mostrar_habilidade_ataque(self, dados_ataque):
+        string_hab_ataque = ""
+        for habilidade in dados_ataque:
+            string_hab_ataque = string_hab_ataque + "ID: " + habilidade["id"] + '\n'
+            string_hab_ataque = string_hab_ataque + "NOME: " + habilidade["nome"] + '\n'
+            string_hab_ataque = string_hab_ataque + "DESCRIÇÃO: " + habilidade["descricao"] + '\n'
+            string_hab_ataque = string_hab_ataque + "CUSTO DE STAMINA: " + habilidade["custo"] + '\n'
+            string_hab_ataque = string_hab_ataque + "DANO DA HABILIDADE: " + habilidade["dano"] + '\n'
+            string_hab_ataque = string_hab_ataque + '\n'
+
+        sg.Popup('░▒▓█ LISTA DE HABILIDADES DE ATAQUE █▓▒░', string_hab_ataque)
+
+    def mostrar_habilidade_defesa(self, dados_defesa):
+        string_hab_defesa = ""
+        for habilidade in dados_defesa:
+            string_hab_defesa = string_hab_defesa + "ID: " + habilidade["id"] + '\n'
+            string_hab_defesa = string_hab_defesa + "NOME: " + habilidade["nome"] + '\n'
+            string_hab_defesa = string_hab_defesa + "DESCRIÇÃO: " + habilidade["descricao"] + '\n'
+            string_hab_defesa = string_hab_defesa + "CUSTO DE STAMINA: " + habilidade["custo"] + '\n'
+            string_hab_defesa = string_hab_defesa + "TAXA DE DEFESA: " + habilidade["taxa_defesa"] + '\n'
+            string_hab_defesa = string_hab_defesa + '\n'
+
+        sg.Popup('░▒▓█ LISTA DE HABILIDADES DE DEFESA █▓▒░', string_hab_defesa)
+
+    def mostrar_habilidade_esquiva(self, dados_esquiva):
+        string_hab_esquiva = ""
+        for habilidade in dados_esquiva:
+            string_hab_esquiva = string_hab_esquiva + "ID: " + habilidade["id"] + '\n'
+            string_hab_esquiva = string_hab_esquiva + "NOME: " + habilidade["nome"] + '\n'
+            string_hab_esquiva = string_hab_esquiva + "DESCRIÇÃO: " + habilidade["descricao"] + '\n'
+            string_hab_esquiva = string_hab_esquiva + "CUSTO DE STAMINA: " + habilidade["custo"] + '\n'
+            string_hab_esquiva = string_hab_esquiva + "TAXA DE ESQUIVA: " + habilidade["taxa_esquiva"] + '\n'
+            string_hab_esquiva = string_hab_esquiva + '\n'
+
+        sg.Popup('░▒▓█ LISTA DE HABILIDADES DE ESQUIVA █▓▒░', string_hab_esquiva)
+
     def obtem_id_ataque(self):
-        resposta = self.le_num_inteiro("Digite o ID da Habilidade de ataque que deseja adicionar ao seu "
-                                       "boxeador ou 0 para voltar: ", [0, 1, 2, 3])
-        print()
-        return resposta
+        sg.ChangeLookAndFeel('DarkTanBlue')
+        layout = [
+            [sg.Text('=== Seleção de habilidades ===', font=("Lucida", 25))],
+            [sg.Text('Digite o ID da habilidade que deseja adicionar ao seu boxeador:', font=("Lucida", 15))],
+            [sg.Text('ID:', size=(15, 1)), sg.InputText('id', key='id_ataque')],
+            [sg.Button('Confirmar'), sg.Cancel('Voltar')]
+        ]
+        self.__window = sg.Window('id ataque').Layout(layout)
+
+        button, values = self.open()
+        identificador = self.le_num_inteiro_entre(values['identificador'], 1, 3)
+        self.close()
+        return identificador
 
     def obtem_id_defesa(self):
-        resposta = self.le_num_inteiro("Digite o ID da Habilidade de defesa que deseja adicionar ao seu "
-                                       "boxeador ou 0 para voltar: ", [0, 4, 5, 6])
-        print()
-        return resposta
+        sg.ChangeLookAndFeel('DarkTanBlue')
+        layout = [
+            [sg.Text('=== Seleção de habilidades ===', font=("Lucida", 25))],
+            [sg.Text('Digite o ID da habilidade que deseja adicionar ao seu boxeador:', font=("Lucida", 15))],
+            [sg.Text('ID:', size=(15, 1)), sg.InputText('id', key='id_defesa')],
+            [sg.Button('Confirmar'), sg.Cancel('Voltar')]
+        ]
+        self.__window = sg.Window('id defesa').Layout(layout)
+
+        button, values = self.open()
+        identificador = self.le_num_inteiro_entre(values['identificador'], 4, 6)
+        self.close()
+        return identificador
 
     def obtem_id_esquiva(self):
-        resposta = self.le_num_inteiro("Digite o ID da Habilidade de esquiva que deseja adicionar ao seu "
-                                       "boxeador ou 0 para voltar: ", [0, 7, 8, 9])
-        print()
-        return resposta
+        sg.ChangeLookAndFeel('DarkTanBlue')
+        layout = [
+            [sg.Text('=== Seleção de habilidades ===', font=("Lucida", 25))],
+            [sg.Text('Digite o ID da habilidade que deseja adicionar ao seu boxeador:', font=("Lucida", 15))],
+            [sg.Text('ID:', size=(15, 1)), sg.InputText('id', key='id_esquiva')],
+            [sg.Button('Confirmar'), sg.Cancel('Voltar')]
+        ]
+        self.__window = sg.Window('id esquiva').Layout(layout)
+
+        button, values = self.open()
+        identificador = self.le_num_inteiro_entre(values['identificador'], 7, 9)
+        self.close()
+
+        return identificador

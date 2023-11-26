b25557bdf26a342b0d4d458f407831e0de9c43cb
Cleverson Borges <143210642+Cleverson-Borges@users.noreply.github.com>
2023-11-26 16:44:31 -0300

    Update tela_central.py

--- Telas/tela_central.py
+++ Telas/tela_central.py
@@ -4,6 +4,7 @@ import PySimpleGUI as sg
 class TelaCentral(TelaAbstrata):
     def __init__(self):
         self.__window = None
+        self.init_opcoes()
 
     def mostrar_mensagem(self, mensagem):
         print(mensagem)
@@ -16,28 +17,7 @@ class TelaCentral(TelaAbstrata):
         self.__window.Close()
 
     def tela_opcoes(self):
-        sg.ChangeLookAndFeel('DarkTanBlue')
-        layout = [
-            [sg.Column(
-                [
-                    [sg.Text('   ░▒▓█ PUNCH CLUB █▓▒░   ', font=("Lucida", 25, 'bold'))],
-                    [sg.Text('                          ', font=("Lucida", 15, 'bold'))],
-                    [sg.Button('CADASTRO DE BOXEADOR', size=(20, 2), key='1', button_color=('white', 'black'),
-                               border_width=0,font=("Lucida", 12, 'bold'))],
-                    [sg.Button('CADASTRO DE TORNEIO', size=(20, 2), key='2', button_color=('white', 'black'),
-                               border_width=0, font=("Lucida", 12, 'bold'))],
-                    [sg.Text('                          ')],
-                    [sg.Button('  ░▓ START GAME ▓░   ', size=(20, 2), key='3', button_color=('white', 'black'),
-                               border_width=0, font=("Lucida", 12, 'bold'))],
-                    [sg.Cancel('SAIR', size=(20, 2), key='4', font=("Lucida", 12, 'bold'))],
-                ],
-                element_justification='center'
-            )]
-        ]
-        self.__window = sg.Window('Cadastro de contas').Layout(layout)
-
-    def inicia_opcoes(self):
-        self.tela_opcoes()
+        self.init_opcoes()
         button, values = self.__window.Read()
         opcao = 0
         if button == '1':
@@ -46,8 +26,30 @@ class TelaCentral(TelaAbstrata):
             opcao = 2
         if button == '3':
             opcao = 3
-        if button == '4':
-            opcao = 4
+        if button == '0':
+            opcao = 0
 
         self.close()
         return opcao
+
+    def init_opcoes(self):
+        sg.ChangeLookAndFeel('DarkTanBlue')
+        layout = [
+            [sg.Column(
+                [
+                    [sg.Text('   ░▒▓█ PUNCH CLUB █▓▒░   ', font=("Lucida", 25, 'bold'))],
+                    [sg.Text('                          ', font=("Lucida", 15, 'bold'))],
+                    [sg.Button('CADASTRO DE BOXEADOR', size=(23, 2), key='1', button_color=('white', 'black'),
+                               border_width=0,font=("Lucida", 12, 'bold'))],
+                    [sg.Button('CADASTRO DE TORNEIO', size=(23, 2), key='2', button_color=('white', 'black'),
+                               border_width=0, font=("Lucida", 12, 'bold'))],
+                    [sg.Text('                          ')],
+                    [sg.Button('  ░▓ START GAME ▓░   ', size=(25, 3), key='3', button_color=('green'),
+                               border_width=0, font=("Lucida", 12, 'bold'))],
+                    [sg.Text('                          ')],
+                    [sg.Cancel('SAIR', size=(20, 2), button_color=('red'), key='0', font=("Lucida", 12, 'bold'))],
+                ],
+                element_justification='center'
+            )]
+        ]
+        self.__window = sg.Window('Cadastro de contas').Layout(layout)

ece135d93c7b140cacd937c867d355a3ac6d4426
Cleverson Borges <143210642+Cleverson-Borges@users.noreply.github.com>
2023-11-26 16:16:18 -0300

    Update tela_caracteristica.py

--- Telas/tela_caracteristica.py
+++ Telas/tela_caracteristica.py
@@ -1,22 +1,12 @@
 from Telas.tela_central import TelaAbstrata
+import PySimpleGUI as sg
 
 
 class TelaCaracteristica(TelaAbstrata):
     def __init__(self):
+        self.__window = None
         pass
 
-    def le_num_inteiro(self, mensagem=" ", ints_validos=None):
-        while True:
-            valor_lido = input(mensagem)
-            try:
-                valor_int = int(valor_lido)
-                if ints_validos and valor_int not in ints_validos:
-                    raise ValueError
-                return valor_int
-            except ValueError:
-                print("Valor inválido!")
-                if ints_validos:
-                    print("Valores válidos: ", ints_validos)
     def tela_cadastro_status(self):
         forca = 10
         esquiva = 10
@@ -25,30 +15,42 @@ class TelaCaracteristica(TelaAbstrata):
         stamina = 15
         incremento_de_pontos = 3
         incremento_de_vida = 10
-        print('Distribua os pontos do seu lutador!')
         for i in range(10):
-            print(f'Você tem {10 - i} ponto(s) para serem distribuídos')
-            print()
-            print(f'1 - Força   - [{forca}] pontos de força atualmente')
-            print(f'2 - Esquiva - [{esquiva}] pontos de esquiva atualmente')
-            print(f'3 - Vida    - [{vida}] pontos de vida atualmente')
-            print(f'4 - Defesa  - [{defesa}] pontos de defesa atualmente')
-            print(f'5 - Stamina - [{stamina}] pontos de stamina atualmente')
-            print()
-            print('0 - Sair')
-            opcao = self.le_num_inteiro("Escolha uma opção: ", [0, 1, 2, 3, 4, 5])
-            print()
-            if opcao == 1:
+            sg.ChangeLookAndFeel('DarkTanBlue')
+            layout = [
+                [sg.Column(
+                    [
+                    [sg.Text('=== DISTRIBUIÇÃO DE PONTOS ===', font=("Lucida", 25))],
+                    [sg.Text('                                      ', font=("Lucida", 15, 'bold'))],
+                    [sg.Text(f'Você tem {10 - i} ponto(s) para distribuir', font=("Lucida", 15, 'bold'))],
+                    [sg.Button('FORÇA (+3)', size=(20, 2), key='1', button_color=('white', 'black'),
+                               border_width=0, font=("Lucida", 12, 'bold'))],
+                    [sg.Button('ESQUIVA (+3)', size=(20, 2), key='2', button_color=('white', 'black'),
+                               border_width=0, font=("Lucida", 12, 'bold'))],
+                    [sg.Button('VIDA (+10)', size=(20, 2), key='3', button_color=('white', 'black'),
+                               border_width=0, font=("Lucida", 12, 'bold'))],
+                    [sg.Button('DEFESA (+3)', size=(20, 2), key='4', button_color=('white', 'black'),
+                               border_width=0, font=("Lucida", 12, 'bold'))],
+                    [sg.Button('STAMINA(+3)', size=(20, 2), key='5', button_color=('white', 'black'),
+                               border_width=0, font=("Lucida", 12, 'bold'))],
+                        ],
+                element_justification='center'
+                )]
+            ]
+            self.__window = sg.Window('=== DISTRIBUIÇÃO DE PONTOS ===').Layout(layout)
+
+            button, values = self.__window.Read()
+            if button == 1:
                 forca = forca + incremento_de_pontos
-            elif opcao == 2:
+            elif button == 2:
                 esquiva = esquiva + incremento_de_pontos
-            elif opcao == 3:
+            elif button == 3:
                 vida = vida + incremento_de_vida
-            elif opcao == 4:
+            elif button == 4:
                 defesa = defesa + incremento_de_pontos
-            elif opcao == 5:
+            elif button == 5:
                 stamina = stamina + incremento_de_pontos
-            elif opcao == 0:
+            elif button == 0:
                 break
 
         return {'forca': forca,

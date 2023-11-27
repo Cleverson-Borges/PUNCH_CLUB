96cfcd029b346b5e3965d308408929ffb8103088
Breno Sayão <101520655+brenisio@users.noreply.github.com>
2023-11-26 23:28:39 -0300

    Update tela_jogo.py

--- Telas/tela_jogo.py
+++ Telas/tela_jogo.py
@@ -10,26 +10,36 @@ class TelaJogo(TelaAbstrata):
         sg.theme('DarkTanBlue')
         self.__window = None
 
-    def le_num_inteiro(self, mensagem=" ", ints_validos=None):
-        while True:
-            valor_lido = input(mensagem)
-            try:
-                valor_int = int(valor_lido)
-                if ints_validos and valor_int not in ints_validos:
-                    raise ValueError
-                return valor_int
-            except ValueError:
-                print("Valor inválido!")
-                if ints_validos:
-                    print("Valores válidos: ", ints_validos)
+    def open(self):
+        button, values = self.__window.Read()
+        return button, values
 
     def obtem_id_torneio(self, lista_ids_validos):
-        print("Escolha pelo ID o torneio que você deseja jogar: ")
-        id_torneio_escolhido = self.le_num_inteiro("Informe o ID do torneio: ",lista_ids_validos)
-        return id_torneio_escolhido
+        sg.ChangeLookAndFeel('DarkTanBlue')
+        layout = [
+            [sg.Text(' ░▒▓█ SELECIONAR ID TORNEIO █▓▒░ ', font=("Lucida", 25))],
+            [sg.Text('Digite o ID do torneio que deseja entrar:', font=("Lucida", 15))],
+            [sg.Text('ID:', size=(15, 1)), sg.InputText('', key='id')],
+            [sg.Button('Confirmar'), sg.Cancel('Voltar')]
+        ]
+        self.__window = sg.Window('Seleciona id do torneio').Layout(layout)
 
-    def tela_luta_padrao(self, vida1, vida2, stamina1, stamina2, nacionalidade1, nacionalidade2,
-                         apelido1, apelido2, round,game_start=False):
+        button, values = self.open()
+        id = self.le_num_inteiro(values['id'], lista_ids_validos)
+        self.close()
+
+        for valor in values:
+            if button == 'Voltar':
+                self.close()
+                return 0
+            if values[valor] == '':
+                self.close()
+                return False
+            if button == 'Confirmar':
+                return int(id)
+
+
+    def tela_luta_padrao(self, vida1, vida2, stamina1, stamina2, nacionalidade1, nacionalidade2, nome1, nome2, apelido1, apelido2, round,game_start=False):
 
         layout = [
             [sg.Image(key='-IMAGE-')],
@@ -41,8 +51,12 @@ class TelaJogo(TelaAbstrata):
                      background_color='black', font=('Helvetica', 14, 'bold'), text_color='white')],
             [sg.Text(apelido1, size=(20, 2), justification='left', expand_x=True,
                      background_color='black', font=('Helvetica', 14, 'bold'), text_color='white'),
+             sg.Text(nome1, size=(20, 2), justification='left', expand_x=True,
+                     background_color='black', font=('Helvetica', 14, 'bold'), text_color='white'),
              sg.Text(apelido2, size=(20, 2), justification='right', expand_x=True,
-                     background_color='black', font=('Helvetica', 14, 'bold'), text_color='white')],
+                     background_color='black', font=('Helvetica', 14, 'bold'), text_color='white')
+             , sg.Text(nome2, size=(20, 2), justification='right', expand_x=True,
+                       background_color='black', font=('Helvetica', 14, 'bold'), text_color='white')],
             [sg.Text(nacionalidade1, background_color='black', justification='left',
                      font=('Helvetica', 14, 'bold'), text_color='white', expand_x=True),
              sg.Text(nacionalidade2, background_color='black', justification='right',
@@ -51,8 +65,7 @@ class TelaJogo(TelaAbstrata):
                     background_color='lightgreen', font=('Helvetica', 14, 'bold'), text_color='black')]
         ]
 
-        window = sg.Window('Tela de Luta', layout, no_titlebar=True, grab_anywhere=True, keep_on_top=True,
-                           finalize=True, location=(600, 300))
+        window = sg.Window('Tela de Luta', layout, no_titlebar=True, grab_anywhere=True, keep_on_top=True, finalize=True)
         image_elem = window['-IMAGE-']
 
         images_base64 = [Imagens.base_64_imagens.boxe_image_base_64,
@@ -74,12 +87,11 @@ class TelaJogo(TelaAbstrata):
                     else:
                         window.close()
 
-    def tela_luta_escolha_habilidade(self, vida1, vida2, stamina1, stamina2, mensagem_habilidade_escolhida,
-                                     habilidades_usuario):
+    def tela_luta_escolha_habilidade(self, vida1, vida2, stamina1, stamina2, mensagem_habilidade_escolhida, habilidades_usuario):
 
         layout = [
             [sg.Image(key='-IMAGE-')],
-            [sg.Button('SAIR', key='SAIR', size=(10, 2), button_color=('black', 'pink'))],
+            [sg.Button('SAIR', key='SAIR', size=(10, ), button_color=('black', 'pink'))],
             [sg.Text(f'{vida1} HP   {stamina1} ST', size=(10, 1), justification='left', expand_x=True,
                      background_color='black', font=('Helvetica', 12), text_color='white'),
              sg.Text(f'{vida2} HP   {stamina2} ST', size=(10, 1), justification='right', expand_x=True,
@@ -98,8 +110,7 @@ class TelaJogo(TelaAbstrata):
                 layout.append([botao_habilidade])
                 lista_habilidades_usuario.append(habilidade.id)
             if habilidade.tipo == 'Defesa':
-                botao_habilidade = sg.Button(f'{habilidade.nome}\n(Custo: {habilidade.custo}, '
-                                             f'Defesa: {habilidade.taxa_defesa})',
+                botao_habilidade = sg.Button(f'{habilidade.nome}\n(Custo: {habilidade.custo}, Defesa: {habilidade.taxa_defesa})',
                                              key=habilidade.id,
                                              size=(20, 2),
                                              button_color=('black', 'white'))
@@ -108,8 +119,7 @@ class TelaJogo(TelaAbstrata):
                 lista_habilidades_usuario.append(habilidade.id)
             if habilidade.tipo == 'Esquiva':
 
-                botao_habilidade = sg.Button(f'{habilidade.nome}\n(Custo: {habilidade.custo}, '
-                                             f'Esquiva: {habilidade.taxa_esquiva})',
+                botao_habilidade = sg.Button(f'{habilidade.nome}\n(Custo: {habilidade.custo}, Esquiva: {habilidade.taxa_esquiva})',
                                              key=habilidade.id,
                                              size=(20, 2),
                                              button_color=('black', 'white'))
@@ -117,8 +127,7 @@ class TelaJogo(TelaAbstrata):
                 layout.append([botao_habilidade])
                 lista_habilidades_usuario.append(habilidade.id)
 
-        window = sg.Window('Tela de Luta', layout, no_titlebar=True, grab_anywhere=True,
-                           keep_on_top=True, finalize=True)
+        window = sg.Window('Tela de Luta', layout, no_titlebar=True, grab_anywhere=True, keep_on_top=True, finalize=True)
         image_elem = window['-IMAGE-']
 
         images_base64 = [Imagens.base_64_imagens.boxe_image_base_64,
@@ -178,12 +187,11 @@ class TelaJogo(TelaAbstrata):
         layout = [
             [sg.Image(key='-IMAGE-')],
             [sg.Button('SAIR', key='SAIR', size=(20, 2), button_color=('black', 'pink'))],
-            [sg.Text(f'Parabéns {boxeador_campeao.nome}! Você mereceu!', background_color='black',
-                     justification='right', font=('Helvetica', 14, 'bold'), text_color='white', expand_x=True)]
+            [sg.Text(f'Parabéns {boxeador_campeao.nome}! Você mereceu!', background_color='black', justification='right',
+                     font=('Helvetica', 14, 'bold'), text_color='white', expand_x=True)]
         ]
 
-        window = sg.Window('Tela de Luta', layout, no_titlebar=True, grab_anywhere=True,
-                           keep_on_top=True, finalize=True)
+        window = sg.Window('Tela de Luta', layout, no_titlebar=True, grab_anywhere=True, keep_on_top=True, finalize=True)
         image_elem = window['-IMAGE-']
 
         imagem_campeao = Imagens.base_64_imagens.boxe_image_4_base_64,

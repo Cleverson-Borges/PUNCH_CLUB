361fe652ad385ccd09b8699af5426f9b6090bd60
Cleverson Borges <143210642+Cleverson-Borges@users.noreply.github.com>
2023-11-26 22:43:56 -0300

    Update tela_abstrata.py

--- Telas/tela_abstrata.py
+++ Telas/tela_abstrata.py
@@ -10,22 +10,19 @@ class TelaAbstrata(ABC):
     def print_mensagem(self, mensagem):
         sg.popup('', mensagem)
 
-    def le_num_inteiro(self, mensagem=" ", ints_validos=None):
+    def le_num_inteiro(self, valor_lido, ints_validos=None):
         while True:
-            valor_lido = input(mensagem)
             try:
                 valor_int = int(valor_lido)
-                if ints_validos and valor_int not in ints_validos:
+                if ints_validos is not None and valor_int not in ints_validos:
                     raise ValueError
                 return valor_int
             except ValueError:
-                sg.popup('Valor Invalido!')
-                if ints_validos:
-                    sg.popup('Valores válidos', ints_validos)
+                sg.popup('Valor Inválido! Tente novamente.')
+                valor_lido = sg.popup_get_text('Digite um número inteiro:')
 
-    def le_num_inteiro_entre(self, mensagem=" ", min_valor=None, max_valor=None):
+    def le_num_inteiro_entre(self, valor_lido, min_valor=None, max_valor=None):
         while True:
-            valor_lido = input(mensagem)
             try:
                 valor_int = int(valor_lido)
                 if (min_valor is not None and valor_int < min_valor) or (
@@ -35,10 +32,10 @@ class TelaAbstrata(ABC):
             except ValueError:
                 sg.popup(f'Você deve ter no minímo {min_valor} anos e no maximo {max_valor} '
                          f'anos para poder participar.')
+                valor_lido = sg.popup_get_text('Digite um número inteiro:')
 
-    def le_num_float_entre(self, mensagem=" ", min_valor=None, max_valor=None):
+    def le_num_float_entre(self, valor_lido, min_valor=None, max_valor=None):
         while True:
-            valor_lido = input(mensagem)
             try:
                 valor_float = float(valor_lido)
                 if (min_valor is not None and valor_float < min_valor) or (
@@ -47,16 +44,14 @@ class TelaAbstrata(ABC):
                 return valor_float
             except ValueError:
                 sg.popup(f"O valor deve estar entre {min_valor} e {max_valor}.")
+                valor_lido = sg.popup_get_text('Digite um número inteiro:')
 
-    def le_str_valida(self, mensagem=" ", strs_validas=None):
+    def le_str_valida(self, valor_lido):
         while True:
-            valor_lido = input(mensagem)
-            if len(valor_lido) == 3:
-                if strs_validas and valor_lido not in strs_validas:
-                    sg.popup("Valor incorreto!")
-                    if strs_validas:
-                        sg.popup("Valores válidos: ", ", ".join(strs_validas))
-                else:
-                    return valor_lido.upper()
-            else:
-                sg.popup("A string deve conter exatamente 3 letras.")
+            try:
+                if not isinstance(valor_lido, str) or len(valor_lido) != 3 or not valor_lido.isalpha():
+                    raise ValueError
+                return valor_lido.upper()
+            except ValueError:
+                sg.popup('String Inválida! Digite exatamente 3 letras.')
+                valor_lido = sg.popup_get_text('Digite uma string de 3 letras:')
